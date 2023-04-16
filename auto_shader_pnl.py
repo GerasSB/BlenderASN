0980536b9826b4a01dd1ad9a77610406dbb5aa62
Geras <geras858@gmail.com>
2023-04-15 22:20:10 -0500

    better code style, fixed unregister, updated plugin info

--- auto_shader_pnl.py
+++ auto_shader_pnl.py
@@ -1,11 +1,11 @@
 bl_info = {
     "name": "Auto Shader Nodes",
     "author": "Geras",
-    "version": (1, 0),
-    "blender": (3, 1),
+    "version": (1, 2),
+    "blender": (3, 5),
     "category": "Node",
     "location": "3DView",
-    "description": "QoL features to make previewing finished models easier",
+    "description": "QoL features to help with Double Dash track creation",
     "warning": "",
     "doc_rul": "",
     "tracker_url": "",
@@ -18,7 +18,7 @@ import os
 #--Operators--
 
 #Button 1
-def button_01(context):
+def alpha_shader(context):
     #This entire IF statement creates a shader group on the Blend file and puts everything inside it. Doesn't actually put
     #the shader group inside the material
     if bpy.data.node_groups.find('TransparencyGroup') == -1: #Checks if the blend file already has the group
@@ -90,35 +90,35 @@ def button_01(context):
             
         loopy.active_material.blend_method = 'HASHED' #Sets material blend method to Alpha Hashed
             
-class clssAlphaShader(bpy.types.Operator):
+class AlphaShader(bpy.types.Operator):
     """Give selected meshes the Alpha shader group. Selected mesh needs an image
 texture for this to work"""
     bl_idname = "shdr.alpha"
     bl_label = "Alpha Shader Group"
     
     def execute(self,context):
-        checkSelection(self, context)
-        button_01(context)
+        check_selection(self, context)
+        alpha_shader(context)
         return{'FINISHED'}
 
 #Button 2
-def button_02(context):
-    #This entire IF statement creates a shader group on the Blend file and puts everything inside it. Doesn't actually put
+def vertex_shader(context):
+    #This IF statement creates a shader group on the Blend file and puts everything inside it. Doesn't actually put
     #the shader group inside the material
     if bpy.data.node_groups.find('VertexGroup') == -1: #Checks if the blend file already has the group
         vertexGroup = bpy.data.node_groups.new('VertexGroup', 'ShaderNodeTree') #Creates the group
-        createVrtxGroup(vertexGroup, context)
+        create_vertex_group(vertexGroup, context)
         
-def button_05(context):
+def vertex_alpha_shader(context):
     #This entire IF statement creates a shader group on the Blend file and puts everything inside it. Doesn't actually put
     #the shader group inside the material
     if bpy.data.node_groups.find('VertexTGroup') == -1: #Checks if the blend file already has the group
         vertexGroup = bpy.data.node_groups.new('VertexTGroup', 'ShaderNodeTree') #Creates the group
-        vertexVCnode, vertexBsdf = createVrtxGroup(vertexGroup, context)
+        vertexVCnode, vertexBsdf = create_vertex_group(vertexGroup, context)
         vertexGroup.links.new(vertexVCnode.outputs[1], vertexBsdf.inputs[21])
         
         
-def createVrtxGroup(vertexGroup, context):        
+def create_vertex_group(vertexGroup, context):        
     #Create Transparent Shader inside group and move it
     vertextransparent = vertexGroup.nodes.new('ShaderNodeBsdfTransparent')
     vertextransparent.location = (-200,200)
@@ -163,7 +163,7 @@ def createVrtxGroup(vertexGroup, context):
     vertexGroup.links.new(vertexMixRGB.outputs[0], vertexBsdf.inputs[0])
     return vertexVCnode, vertexBsdf
 
-def linkVrtMats(groupName, nodeName, blendmode, context):
+def link_vertex_nodes(groupName, nodeName, blendmode, context):
     #Creating the node group inside the material
     sO = bpy.context.selected_objects
     for loopy in sO:
@@ -201,19 +201,19 @@ def linkVrtMats(groupName, nodeName, blendmode, context):
             
             loopy.active_material.blend_method = blendmode #Sets material blend method to Alpha Hashed
             
-class clssVertexShader(bpy.types.Operator):
+class VertexShader(bpy.types.Operator):
     """Give selected meshes the Vertex shader group. Selected mesh needs to have
 vertex colors AND image textures for this button to work"""
     bl_idname = "shdr.vertex"
     bl_label = "Vertex Shader Group"
     
     def execute(self,context):
-        checkSelection(self, context)
-        button_02(context)
-        linkVrtMats('VertexGroup','VertexNode', 'HASHED', context)
+        check_selection(self, context)
+        vertex_shader(context)
+        link_vertex_nodes('VertexGroup','VertexNode', 'HASHED', context)
         return{'FINISHED'}
 
-class clssVertexTShader(bpy.types.Operator):
+class VertexTransparencyShader(bpy.types.Operator):
     """Give selected meshes the Vertex Transparency shader group. It allows to
 see transparency in vertex data. Selected mesh needs vertex colors AND image
 textures for this button to work"""
@@ -221,13 +221,13 @@ textures for this button to work"""
     bl_label = "Vertex Transparency Group"
     
     def execute(self,context):
-        checkSelection(self, context)
-        button_05(context)
-        linkVrtMats('VertexTGroup','VertexTNode', 'HASHED', context)
+        check_selection(self, context)
+        vertex_alpha_shader(context)
+        link_vertex_nodes('VertexTGroup','VertexTNode', 'HASHED', context)
         return{'FINISHED'}
 
 #Button 3 - Remove groups
-def button_03(context):
+def remove_groups(context):
     
     #Creating the node inside the material
     sO = bpy.context.selected_objects
@@ -256,15 +256,15 @@ def button_03(context):
             linkin.new(getTex.outputs[0], expbsdf.inputs[0]) #Links color to group node
             linkin.new(expbsdf.outputs[0], exout.inputs[0]) #Links alpha to group node
 
-class clssExport(bpy.types.Operator):
+class ExportSelected(bpy.types.Operator):
     """Blender can't export materials with shader groups, so use this button
 before exporting"""
     bl_idname = "shdr.export"
     bl_label = "Remove Groups (for export)"
     
     def execute(self,context):
-        checkSelection(self, context)
-        button_03(context)
+        check_selection(self, context)
+        remove_groups(context)
         return{'FINISHED'} 
     
 #Color Button       
@@ -274,7 +274,7 @@ def button_04(context):
     scn.view_settings.gamma = 0.4
 
             
-class clssClr(bpy.types.Operator):
+class ColorScene(bpy.types.Operator):
     """Change Exposure and Gamma settings to better match Double Dash's lighting,
 for more accurate viewport previews in Eevee Render Mode"""
     bl_idname = "shdr.color"
@@ -285,44 +285,44 @@ for more accurate viewport previews in Eevee Render Mode"""
         return{'FINISHED'}
 
 # Export DAE Class   
-class class_export_dae(bpy.types.Operator):
+class ExportDae(bpy.types.Operator):
     """Export all selected objects as DAE to a 'course' folder inside the Blender
 file's current directory"""
     bl_idname = "shdr.exportdae"
     bl_label = "Export Selection as DAE"
     
     def execute(self,context):
-        checkSelection(self, context)
-        exportChecks(self, context, '.dae')
+        check_selection(self, context)
+        export_checks(self, context, '.dae')
         return{'FINISHED'}
 
-class class_export_obj(bpy.types.Operator):
+class ExportObj(bpy.types.Operator):
     """Export all selected objects as OBJ to a 'course_collision' folder inside
 the Blender file's current directory"""
     bl_idname = "shdr.exportobj"
     bl_label = "Export Selection as OBJ"
     
     def execute(self,context):
-        checkSelection(self, context)
-        exportChecks(self, context, '.obj')
+        check_selection(self, context)
+        export_checks(self, context, '.obj')
         return{'FINISHED'}
 
-def daeExport(pathname, context):
-        button_03(context)
+def dae_export(pathname, context):
+        remove_groups(context)
         bpy.ops.wm.collada_export(filepath=pathname, selected=True, triangulate=True)
 
-def objExport(pathname, context):
-        button_03(context)
+def obj_export(pathname, context):
+        remove_groups(context)
         bpy.ops.wm.obj_export(filepath=pathname, export_selected_objects=True, export_triangulated_mesh=True, check_existing=False)
 
-def setPathname(self, context, type, course_name):
+def set_pathname(self, context, type, course_name):
     pathname = bpy.context.blend_data.filepath
     filename = bpy.path.basename(bpy.context.blend_data.filepath)
     if type == '.dae':
         pathname = pathname.removesuffix(filename) + 'course\\'
         filename = course_name + '_course.dae'
         pathname = pathname + filename
-        daeExport(pathname, context)
+        dae_export(pathname, context)
         self.report({'INFO'}, 'File exported to ' + pathname)
         
     elif type == '.obj':
@@ -331,11 +331,11 @@ def setPathname(self, context, type, course_name):
             os.makedirs(pathname)
         filename = course_name + '_course.obj'
         pathname = pathname + filename
-        objExport(pathname, context)
+        obj_export(pathname, context)
         self.report({'INFO'}, 'File exported to ' + pathname)
         
 # Some checks before exporting DAE and returns the path it'll export to
-def exportChecks(self, context, type):
+def export_checks(self, context, type):
     course_name = context.scene.my_tool.course_name
     if bpy.context.blend_data.filepath != '':
         if bpy.context.selected_objects == []:
@@ -343,14 +343,14 @@ def exportChecks(self, context, type):
         elif course_name == '':
             self.report({'ERROR'}, "Please enter your course's name")
         else:
-            setPathname(self, context, type, course_name)
+            set_pathname(self, context, type, course_name)
     else:
         self.report({'ERROR'}, 'Save your Blender project somewhere before exporting')
         
 #--Panel--
 from bpy.types import Panel
 
-def checkSelection(self, context):
+def check_selection(self, context):
     for i in bpy.context.selected_objects:
         if i.data.name not in bpy.data.meshes.keys():
             self.report({'ERROR'}, "You're selecting a non-mesh object. Aborting.")
@@ -359,45 +359,23 @@ def checkSelection(self, context):
             self.report({'ERROR'}, "One of your meshes doesn't have a material assigned. Aborting.")
             raise ValueError("One of your meshes doesn't have a material assigned")
 
-def roadtypeInfo(self, context):
-    roadtypes = {
-    "Roadtype_0x00": "Medium offroad, mud",
-    "Roadtype_0x01": "Normal road",
-    "Roadtype_0x02": "Kart and item wall",
-    "Roadtype_0x03": "Medium offroad, grass",
-    "Roadtype_0x04": "Slippery ice road",
-    "Roadtype_0x05": "Road-level deadzone",
-    "Roadtype_0x06": "???",
-    "Roadtype_0x07": "Ramp boost",
-    "Roadtype_0x08": "Boost",
-    "Roadtype_0x09": "Cannon",
-    "Roadtype_0x0A": "Aerial deadzone",
-    "Roadtype_0x0B": "Sand road",
-    "Roadtype_0x0C": "Weak sand offroad",
-    "Roadtype_0x0D": "Pipe teleportation",
-    "Roadtype_0x0E": "Sand deadzone",
-    "Roadtype_0x0F": "Water/lava deadzone",
-    "Roadtype_0x10": "Quicksand sinkhole",
-    "Roadtype_0x11": "Sand deadzone w/ water",
-    "Roadtype_0x12": "Kart-exclusive wall",
-    "Roadtype_0x13": "Heavy offroad",
-    "Roadtype_0x37": "Ramp boost for gaps",
-    "Roadtype_0x47": "Ramp boost for gaps",
-    }
+def roadtype_info(self, context):
+    roadtypes = context.scene.my_tool.roadtypes
     try:
         material_slot = bpy.context.active_object.active_material.name
     except:
         self.report({'ERROR'}, 'No material selected')
-    for type in roadtypes:
-        if type in material_slot:
-            context.scene.my_tool.roadinfo = type.removeprefix('Roadtype_') + " = " + roadtypes[type]
-            return {'FINISHED'}
+    else:
+        for type in roadtypes:
+            if type in material_slot:
+                context.scene.my_tool.roadinfo = type.removeprefix('Roadtype_') + " = " + roadtypes[type]
+                return {'FINISHED'}
     self.report({'ERROR'}, 'No collision has the name ' + "'" + material_slot + "'")
     context.scene.my_tool.roadinfo = 'Not a valid collision name'
                  
 
 
-class SHD_PT_Panel(Panel):
+class P1AutoShaderNodes(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_label = "Auto Shader Nodes"
@@ -417,7 +395,7 @@ class SHD_PT_Panel(Panel):
         #Remove Button
         self.layout.operator("shdr.export")
         
-class SHD_PT_Panel2(Panel):
+class P2ViewportSettings(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_label = "Viewport Settings"
@@ -428,7 +406,7 @@ class SHD_PT_Panel2(Panel):
         #Alpha Button
         self.layout.operator("shdr.color")
 
-def deleteVerticalGeometry(context):
+def delete_vertical_geometry(context):
     strength = context.scene.my_tool.vertical_strength * 0.24
     bpy.context.view_layer.objects.active = bpy.context.selected_objects[0]
     bpy.ops.object.mode_set(mode='EDIT')
@@ -438,7 +416,7 @@ def deleteVerticalGeometry(context):
     bpy.ops.mesh.select_similar(type='NORMAL', threshold=strength)
     bpy.ops.mesh.delete(type='FACE')
     
-class Delete_Vertical_Geometry(bpy.types.Operator):
+class DeleteVerticalGeometry(bpy.types.Operator):
     """Removes all* vertical geometry from the selected object(s).
     
 * Adjust the strength to taste. The default of 0.63 is recommended"""
@@ -446,22 +424,22 @@ class Delete_Vertical_Geometry(bpy.types.Operator):
     bl_label = "Remove Vertical Geometry"
     
     def execute(self, context):
-        checkSelection(self, context)
-        deleteVerticalGeometry(context)
+        check_selection(self, context)
+        delete_vertical_geometry(context)
         return{'FINISHED'}
     
-class Roadtype_Info(bpy.types.Operator):
+class RoadtypeInfo(bpy.types.Operator):
     """Tells you the type of collision of your currently active material"""
     bl_idname = "shdr.roadtypeinfo"
     bl_label = "Roadtype Info"
     
     def execute(self, context):
-        roadtypeInfo(self, context)
+        roadtype_info(self, context)
         return{'FINISHED'}
     
 
 # Export Panel
-class SHD_PT_Panel3(Panel):
+class P3Export(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_label = "Export"
@@ -485,7 +463,7 @@ class SHD_PT_Panel3(Panel):
         self.layout.operator("shdr.exportobj")
 
 # Tools Panel
-class SHD_PT_Panel4(Panel):
+class P4CollisionTools(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_label = "Collision Tools"
@@ -510,39 +488,66 @@ class MyProperties(bpy.types.PropertyGroup):
     
     roadinfo : bpy.props.StringProperty(name='')
 
+    roadtypes = {
+        "Roadtype_0x00": "Medium offroad, mud",
+        "Roadtype_0x01": "Normal road",
+        "Roadtype_0x02": "Kart and item wall",
+        "Roadtype_0x03": "Medium offroad, grass",
+        "Roadtype_0x04": "Slippery ice road",
+        "Roadtype_0x05": "Road-level deadzone",
+        "Roadtype_0x06": "???",
+        "Roadtype_0x07": "Ramp boost",
+        "Roadtype_0x08": "Boost",
+        "Roadtype_0x09": "Cannon",
+        "Roadtype_0x0A": "Aerial deadzone",
+        "Roadtype_0x0B": "Sand road",
+        "Roadtype_0x0C": "Weak sand offroad",
+        "Roadtype_0x0D": "Pipe teleportation",
+        "Roadtype_0x0E": "Sand deadzone",
+        "Roadtype_0x0F": "Water/lava deadzone",
+        "Roadtype_0x10": "Quicksand sinkhole",
+        "Roadtype_0x11": "Sand deadzone w/ water",
+        "Roadtype_0x12": "Kart-exclusive wall",
+        "Roadtype_0x13": "Heavy offroad",
+        "Roadtype_0x37": "Ramp boost for gaps",
+        "Roadtype_0x47": "Ramp boost for gaps"
+    }
 
-        
+
+
 #--Blender stuff--        
 def register():
-    bpy.utils.register_class(SHD_PT_Panel)
-    bpy.utils.register_class(clssAlphaShader)
-    bpy.utils.register_class(clssVertexShader)
-    bpy.utils.register_class(clssVertexTShader)
-    bpy.utils.register_class(clssExport)
-    bpy.utils.register_class(SHD_PT_Panel2)
-    bpy.utils.register_class(SHD_PT_Panel3)
-    bpy.utils.register_class(SHD_PT_Panel4)
-    bpy.utils.register_class(clssClr)
-    bpy.utils.register_class(class_export_dae)
-    bpy.utils.register_class(class_export_obj)
-    bpy.utils.register_class(Delete_Vertical_Geometry)
-    bpy.utils.register_class(Roadtype_Info)
+    bpy.utils.register_class(P1AutoShaderNodes)
+    bpy.utils.register_class(AlphaShader)
+    bpy.utils.register_class(VertexShader)
+    bpy.utils.register_class(VertexTransparencyShader)
+    bpy.utils.register_class(ExportSelected)
+    bpy.utils.register_class(P2ViewportSettings)
+    bpy.utils.register_class(P3Export)
+    bpy.utils.register_class(P4CollisionTools)
+    bpy.utils.register_class(ColorScene)
+    bpy.utils.register_class(ExportDae)
+    bpy.utils.register_class(ExportObj)
+    bpy.utils.register_class(DeleteVerticalGeometry)
+    bpy.utils.register_class(RoadtypeInfo)
     bpy.utils.register_class(MyProperties)
     bpy.types.Scene.my_tool = bpy.props.PointerProperty(type=MyProperties)
 
 def unregister():
-    bpy.utils.register_class(SHD_PT_Panel)
-    bpy.utils.register_class(clssAlphaShader)
-    bpy.utils.register_class(clssVertexShader)
-    bpy.utils.register_class(clssVertexTShader)
-    bpy.utils.register_class(clssExport)
-    bpy.utils.register_class(SHD_PT_Panel2)
-    bpy.utils.register_class(SHD_PT_Panel3)
-    bpy.utils.register_class(SHD_PT_Panel4)
-    bpy.utils.register_class(clssClr)
-    bpy.utils.register_class(class_export_dae)
-    bpy.utils.register_class(Delete_Vertical_Geometry)
-    bpy.utils.register_class(class_export_obj)
+    bpy.utils.unregister_class(P1AutoShaderNodes)
+    bpy.utils.unregister_class(AlphaShader)
+    bpy.utils.unregister_class(VertexShader)
+    bpy.utils.unregister_class(VertexTransparencyShader)
+    bpy.utils.unregister_class(ExportSelected)
+    bpy.utils.unregister_class(P2ViewportSettings)
+    bpy.utils.unregister_class(P3Export)
+    bpy.utils.unregister_class(P4CollisionTools)
+    bpy.utils.unregister_class(ColorScene)
+    bpy.utils.unregister_class(ExportDae)
+    bpy.utils.unregister_class(RoadtypeInfo)
+    bpy.utils.unregister_class(DeleteVerticalGeometry)
+    bpy.utils.unregister_class(ExportObj)
+    bpy.utils.unregister_class(MyProperties)
     del bpy.types.Scene.my_tool
     
 if __name__ == "__main__":
